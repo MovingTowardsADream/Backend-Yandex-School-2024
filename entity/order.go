b2755b2184a82844173a31d950739dbe4bd86b0c
MovingTowardsADream <smirnov.matveychik@mail.ru>
2024-03-31 22:15:34 +0300

    couuriers

--- entity/order.go
+++ entity/order.go
@@ -1,8 +1,10 @@
 package entity
 
+import "github.com/lib/pq"
+
 type Order struct {
-	Id             int      `json:"id"`
-	Weight         int      `json:"weight"`
-	District       int      `json:"district"`
-	ConvenientTime []string `json:"convenient_time"`
+	Id             int            `json:"id"`
+	Weight         int            `json:"weight"`
+	District       int            `json:"district"`
+	ConvenientTime pq.StringArray `json:"convenient_time"`
 }
