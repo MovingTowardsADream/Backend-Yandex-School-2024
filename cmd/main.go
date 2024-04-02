c1b4a584edc5149c5f9b56207bc50a1bc85bad86
MovingTowardsADream <smirnov.matveychik@mail.ru>
2024-04-02 13:41:46 +0300

    yandex-lavka-API

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 	"log"
+	"os"
 	yandex_lavka "yandex-lavka"
 	"yandex-lavka/pkg/handler"
 	"yandex-lavka/pkg/repository"
@@ -13,13 +16,21 @@ import (
 func main() {
 	logrus.SetFormatter(new(logrus.JSONFormatter))
 
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("Failed reading config")
+	}
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("Failed reading db password")
+	}
+
 	db, err := repository.NewPostgresDB(repository.ConfigDB{
-		Host:     "localhost",
-		Port:     "5432",
-		Username: "postgres",
-		Password: "qwerty",
-		DBName:   "postgres",
-		SSLMode:  "disable",
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.name"),
+		SSLMode:  viper.GetString("db.ssl_mode"),
 	})
 
 	if err != nil {
@@ -31,7 +42,13 @@ func main() {
 	handlers := handler.NewHandler(serv)
 
 	server := new(yandex_lavka.Server)
-	if err := server.Run("8080", handlers.InitHandler()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitHandler()); err != nil {
 		log.Fatal("Error server")
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
--- server.go
+++ server.go
@@ -2,24 +2,38 @@ package yandex_lavka
 
 import (
 	"context"
+	"golang.org/x/time/rate"
 	"net/http"
 	"time"
 )
 
 type Server struct {
 	httpServer *http.Server
+	limiter    *rate.Limiter
 }
 
 func (s *Server) Run(port string, handler http.Handler) error {
+	s.limiter = rate.NewLimiter(rate.Every(time.Second/10), 1) // Ограничение до 10 запросов в секунду
 	s.httpServer = &http.Server{
 		Addr:           ":" + port,
-		Handler:        handler,
+		Handler:        s.limitMiddleware(handler),
 		MaxHeaderBytes: 1 << 20,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 	}
 
 	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) limitMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !s.limiter.Allow() {
+			http.Error(w, http.StatusText(http.StatusTooManyRequests), http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
 
 func (s *Server) Shutdown(ctx context.Context) error {
